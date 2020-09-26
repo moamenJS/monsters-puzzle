3e70f3be28be56b9a25e0424025d62e05a326ff7
moamenJS <moamenmedleg@hotmail.com>
2020-09-26 18:20:09 +0300

    Updates

---
+++ precache-manifest.8981a740925eed6b9fc2cd2eb01bb5d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1fd93b54ef527e209c8ad032f879b85",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "204326069a6fb8a43845",
+    "url": "/monsters-rolodex/static/css/main.66de2ebb.chunk.css"
+  },
+  {
+    "revision": "2aa89dde0859bdbbf2dc",
+    "url": "/monsters-rolodex/static/js/2.c3b51709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.c3b51709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "204326069a6fb8a43845",
+    "url": "/monsters-rolodex/static/js/main.937f24ac.chunk.js"
+  },
+  {
+    "revision": "8d06a3586d13b63673c9",
+    "url": "/monsters-rolodex/static/js/runtime-main.f22b6454.js"
+  }
+]);
